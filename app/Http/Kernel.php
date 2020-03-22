03ab6c3c2671f7595cde0614ec7ed15dbe4886bc
Emad Wahhab Al-Kabi <emadalkabi1994@gmail.com>
2020-03-22 12:18:16 +0300

    update Kernal

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\AuthKey;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -41,7 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-//            AuthKey::class
+            \App\Http\Middleware\AuthKey::class
         ],
     ];
 
